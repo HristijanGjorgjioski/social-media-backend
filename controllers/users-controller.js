391011c558afc9cb2fdd7f51b0c0fc9baccfc2a0
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-03-28 23:10:19 +0200

    Worked on signin function in user controller

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -24,4 +24,25 @@ export const register = async (req, res) => {
         res.status(500).json({ message: "Something went wrong!" });
         console.log(error);
     }
+};
+
+export const signin = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const userExsist = User.findOne({ email });
+
+        if(!userExsist) return res.status(400).json({ message: "User does not exsist" });
+
+        const isPasswordCorrect = bcrypt.compare(password, userExsist.password);
+
+        if(!isPasswordCorrect) return res.status(400).json({ message: "Invalid credentials" });
+
+        const token = jwt.sign({ email: userExsist.email, id: userExsist._id }, secret, { expiresIn: "1h" } );
+
+        res.status(200).json({ result: userExsist, token });
+    } catch (error) {
+        res.status(500).json({ message: "Something went wrong!" });
+        console.log(error);
+    }
 }
--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 
-import { register } from '../controllers/users-controller.js';
+import { register, signin } from '../controllers/users-controller.js';
 
 const router = express.Router();
 
 router.post('/register', register);
+router.post('/signin', signin);
 
 export default router;
