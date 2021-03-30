d0178acd38b9688aaf88ed03a45f8b98f4e3f168
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-03-30 23:11:36 +0200

    Successfully creating posts and storing them to the database!

--- controllers/posts-controller.js
+++ controllers/posts-controller.js
@@ -1,10 +1,10 @@
 import Post from '../models/posts-model.js';
 
 export const createPost = async (req, res) => {
-    const { post } = req.body;
+    const { description, selectedFile, creator } = req.body;
 
     try {
-        const result = await Post.create(post);
+        const result = await Post.create({ creator, description, photo: selectedFile });
 
         res.status(201).json({ result });
     } catch (error) {
