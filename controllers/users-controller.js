7f02a05c1fdd52045fb470af058669e6ce1d2bd9
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-03-30 12:15:03 +0200

    Authentication is done!!

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -6,7 +6,7 @@ import User from '../models/users-model.js';
 const secret = "social-media";
 
 export const register = async (req, res) => {
-    const { email, password, firstName, lastName } = req.body[0];
+    const { email, password, firstName, lastName } = req.body;
 
     try {
         const oldUser = await User.findOne({ email });
@@ -28,21 +28,20 @@ export const register = async (req, res) => {
 
 export const signin = async (req, res) => {
     const { email, password } = req.body;
-
+  
     try {
-        const userExsist = User.findOne({ email });
-
-        if(!userExsist) return res.status(400).json({ message: "User does not exsist" });
-
-        const isPasswordCorrect = bcrypt.compare(password, userExsist.password);
-
-        if(!isPasswordCorrect) return res.status(400).json({ message: "Invalid credentials" });
-
-        const token = jwt.sign({ email: userExsist.email, id: userExsist._id }, secret, { expiresIn: "1h" } );
-
-        res.status(200).json({ result: userExsist, token });
-    } catch (error) {
-        res.status(500).json({ message: "Something went wrong!" });
-        console.log(error);
+      const oldUser = await User.findOne({ email });
+  
+      if (!oldUser) return res.status(404).json({ message: "User doesn't exist" });
+  
+      const isPasswordCorrect = await bcrypt.compare(password, oldUser.password);
+  
+      if (!isPasswordCorrect) return res.status(400).json({ message: "Invalid credentials" });
+  
+      const token = jwt.sign({ email: oldUser.email, id: oldUser._id }, secret, { expiresIn: "1h" });
+  
+      res.status(200).json({ token, result: oldUser });
+    } catch (err) {
+      res.status(500).json({ message: "Something went wrong" });
     }
-}
+  };
